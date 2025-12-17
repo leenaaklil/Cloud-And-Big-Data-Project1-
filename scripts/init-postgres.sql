43371e976e6108f8b296bc8e34e0be32a822ca9e
leena <144286314+leenaaklil@users.noreply.github.com>
2025-12-17 14:00:05 +0100

    Update init-postgres.sql

--- scripts/init-postgres.sql
+++ scripts/init-postgres.sql
@@ -1,7 +1,21 @@
-CREATE TABLE IF NOT EXISTS nyc_taxi_analytics (
-    VendorID INT,
-    avg_fare FLOAT,
-    total_trips INT,
-    avg_tip_pct FLOAT,
-    avg_distance FLOAT
+CREATE TABLE nyc_taxi_trips_cleaned (
+    vendorid INTEGER,
+    tpep_pickup_datetime TIMESTAMP,
+    tpep_dropoff_datetime TIMESTAMP,
+    passenger_count INTEGER,
+    trip_distance FLOAT,
+    ratecodeid INTEGER,
+    store_and_fwd_flag TEXT,
+    pulocationid INTEGER,
+    dolocationid INTEGER,
+    payment_type INTEGER,
+    fare_amount FLOAT,
+    extra FLOAT,
+    mta_tax FLOAT,
+    tip_amount FLOAT,
+    tolls_amount FLOAT,
+    improvement_surcharge FLOAT,
+    total_amount FLOAT,
+    congestion_surcharge FLOAT
 );
+
